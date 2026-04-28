cc7048a8ad1177183ef29bce70e4573a406c2557
Edwin Carpio <twinsanity864@hotmail.com>
2026-04-28 11:25:27 -0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://crm-app-eta-green.vercel.app/api',
-    ],
+    origin: ['http://localhost:3000', 'https://crm-app-eta-green.vercel.app'],
     credentials: true,
   });
 
